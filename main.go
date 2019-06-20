c122b8989181130b70c2ac0ad3caf0b7de6156a4
Sebastian Waisbrot <seppo0010@gmail.com>
2019-06-19 23:00:45 -0300

    Support GIFs

--- main.go
+++ main.go
@@ -13,9 +13,11 @@ import (
 
 var stickerDict map[string][]string = map[string][]string{}
 var photoDict map[string][]string = map[string][]string{}
+var gifDict map[string][]string = map[string][]string{}
 
 const STICKER_JSON_PATH = "stickers.json"
 const PHOTO_JSON_PATH = "photos.json"
+const GIF_JSON_PATH = "gif.json"
 
 func addItem(m *tb.Message, fromDict *sync.Map, toDict map[string][]string, path string) (bool, error) {
 	fileIDif, found := fromDict.Load(m.Chat.ID)
@@ -62,17 +64,36 @@ func main() {
 	if err == nil {
 		json.NewDecoder(f).Decode(&photoDict)
 	}
+	f, err = os.Open(GIF_JSON_PATH)
+	if err == nil {
+		json.NewDecoder(f).Decode(&gifDict)
+	}
 
 	addingStickers := &sync.Map{}
 	addingPhotos := &sync.Map{}
+	addingGifs := &sync.Map{}
 	b.Handle(tb.OnSticker, func(m *tb.Message) {
 		b.Send(m.Sender, "cool sticker, bro... send me some keywords (separated by commas)")
 		addingStickers.Store(m.Chat.ID, m.Sticker.File.FileID)
+		addingPhotos.Delete(m.Chat.ID)
+		addingGifs.Delete(m.Chat.ID)
 	})
 
 	b.Handle(tb.OnPhoto, func(m *tb.Message) {
 		b.Send(m.Sender, "cool photo, bro... send me some keywords (separated by commas)")
 		addingPhotos.Store(m.Chat.ID, m.Photo.File.FileID)
+		addingStickers.Delete(m.Chat.ID)
+		addingGifs.Delete(m.Chat.ID)
+	})
+
+	b.Handle(tb.OnDocument, func(m *tb.Message) {
+		if m.Document.MIME != "video/mp4" {
+			return
+		}
+		b.Send(m.Sender, "cool video, bro... send me some keywords (separated by commas)")
+		addingGifs.Store(m.Chat.ID, m.Document.File.FileID)
+		addingStickers.Delete(m.Chat.ID)
+		addingPhotos.Delete(m.Chat.ID)
 	})
 
 	b.Handle(tb.OnText, func(m *tb.Message) {
@@ -82,7 +103,10 @@ func main() {
 			path string
 		}
 		for _, ft := range []fromTo{
-			fromTo{addingStickers, stickerDict, STICKER_JSON_PATH}, fromTo{addingPhotos, photoDict, PHOTO_JSON_PATH}} {
+			fromTo{addingStickers, stickerDict, STICKER_JSON_PATH},
+			fromTo{addingPhotos, photoDict, PHOTO_JSON_PATH},
+			fromTo{addingGifs, gifDict, GIF_JSON_PATH},
+		} {
 			added, err := addItem(m, ft.from, ft.to, ft.path)
 			if err != nil {
 				log.Print(err)
@@ -96,6 +120,15 @@ func main() {
 	})
 
 	b.Handle(tb.OnQuery, func(q *tb.Query) {
+		gifs := map[string]bool{}
+		for word, fileIDs := range gifDict {
+			if strings.HasPrefix(word, strings.ToLower(q.Text)) {
+				for _, fileID := range fileIDs {
+					gifs[fileID] = true
+				}
+			}
+		}
+
 		stickers := map[string]bool{}
 		for word, fileIDs := range stickerDict {
 			if strings.HasPrefix(word, strings.ToLower(q.Text)) {
@@ -114,7 +147,12 @@ func main() {
 			}
 		}
 
-		results := make(tb.Results, 0, len(stickers)+len(photos))
+		results := make(tb.Results, 0, len(gifs)+len(stickers)+len(photos))
+		for fileID, _ := range gifs {
+			res := &tb.Mpeg4GifResult{Cache: fileID}
+			res.SetResultID(fileID)
+			results = append(results, res)
+		}
 		for fileID, _ := range stickers {
 			res := &tb.StickerResult{Cache: fileID}
 			res.SetResultID(fileID)
